46783c3b7b6e47d2239410740b9ecf469cc08e69
Arasyilmaz1 <ayilmaz26@cornellcollege.edu>
2025-03-23 20:13:05 -0500

    action class

---
+++ Action.py
@@ -0,0 +1,11 @@
+
+#An action of this game is just a tuple (a,b), which means remove b pebbles from ath pile.
+class Action:
+  def __init__(self, action):
+    if type(action) is (not list or not tuple):
+      raise TypeError('Argument should be a tuple or a list')
+    else:
+      self.action = tuple(action)
+
+  def __str__(self):
+    return '(' + str(self.action[0]) + ',' + str(self.action[1]) + ')'
