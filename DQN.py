93365130bfeeccb816215c5a02a7124b2a03e8b0
Arasyilmaz1 <ayilmaz26@cornellcollege.edu>
2025-03-27 21:28:13 -0500

    DQN Update

--- DQN.py
+++ DQN.py
@@ -3,7 +3,7 @@ import random
 from operator import itemgetter
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from keras import layers
 from collections import deque
 from Agent import Agent
 from State import State
@@ -13,6 +13,8 @@ class DQN(Agent):
     self.epsilon = epsilon
     self.alpha = alpha
     self.gamma = gamma
+    self.max_pebbles = max_pebbles
+    self.max_piles = max_piles
 
 
     self.model = DQN._create_q_model(max_pebbles,max_piles)
@@ -29,10 +31,10 @@ class DQN(Agent):
     layer0 = layers.Flatten(input_shape=(3, 3), name="flatten")(inputs)
     layer1 = layers.Dense(128, activation="relu", name="dense_1")(layer0)
     layer2 = layers.Dense(128, activation="relu", name="dense_2")(layer1)
-    action = layers.Dense(21,activation='linear', output= "output_linear")(layer2)
+    action = layers.Dense(max_pebbles*max_piles,activation='linear', name= "output_linear")(layer2)
     return keras.Model(inputs=inputs, outputs=action)
 
-  def _decode_state(self,state_converted):
+  def _decode_state(state_converted):
     lst = []
     for i in range(len(state_converted)-1):
       num = 0
@@ -40,9 +42,84 @@ class DQN(Agent):
         if state_converted[i][j]:
           num += 2**j
       lst.append(num)
+    return lst
+  
+  def _encode_heaps(self,heaps):
+    tensor_size = max(max(heaps).bit_length(),len(heaps))
+    square =[]
+    for heap in heaps:
+      representation = bin(heap)[-1:1:-1]
+      lst = [char == '1' for char in representation]
+      while len(lst) < tensor_size:
+        lst.append(False)
+      square.append(lst)
+    while len(square) < tensor_size:
+      square.append([False]*tensor_size)
+    return tf.convert_to_tensor(square)
 
-    max_removal = 0
-    for i in range(state_converted[-1]):
-      max_removal += 2**i*int(state_converted[-1][i])
+  def get_qvalues(self, heaps):
+    qvalues = self.model(
+        tf.expand_dims(self._encode_heaps(heaps), axis=0),
+        training=False
+        )
+    return qvalues[0]
 
-    return State(lst, max_removal)
+  def _pick_best_move(self, heaps, max_removal):
+    qvls = self.get_qvalues(heaps)
+    qvls_possible = qvls.numpy()
+    for i in range(len(qvls)):
+      if i%self.max_pebbles+1 > max_removal or i%self.max_pebbles+1 > heaps[i//self.max_pebbles]:
+        qvls_possible[i] = -1
+    qvalues_indexed = [(i,qvl) for i, qvl in enumerate(qvls_possible)]
+    random.shuffle(qvalues_indexed)
+    max_index = max(qvalues_indexed, key=itemgetter(1))[0]
+    return qvls_possible[max_index], (max_index //self.max_pebbles+1, max_index % self.max_pebbles +1)
+
+  def get_max_qvalue(self, heaps):
+    best_value, best_move = self._pick_best_move(heaps)
+    return best_value
+
+  def act(self, state):
+    if random.random() < self.epsilon:
+      move = random.choice(state.getActions())
+    else: 
+      move = self._pick_best_move(state.state, state.max_removal)[1]
+    return move
+  
+  def on_step(self, state, action, reward, next_state, debug):
+    internal_action = (action[0]-1)*self.max_pebbles+(action[1]-1)
+    self.replay_buffer.append((
+
+      self._encode_heaps(state.state),
+      internal_action,
+      reward,
+      self._encode_heaps(next_state.state)
+    ))
+    if len(self.replay_buffer) < self.batch_size:
+      return 
+    minibatch_indices = np.random.choice(len(self.replay_buffer), size=self.batch_size)
+    minibatch = [self.replay_buffer[i] for i in minibatch_indices]
+    minibatch_states = np.array([prev[0] for prev in minibatch])
+    minibatch_actions = [prev[1] for prev in minibatch]
+    minibatch_rewards = [prev[2] for prev in minibatch]
+    minibatch_next_states = np.array([prev[3] for prev in minibatch])
+
+        # Compute target
+    target_q_values = self.model_target(minibatch_next_states) * np.any(minibatch_next_states, axis=(1, 2)).astype(int)[:, None]
+    target_term = minibatch_rewards + self.gamma * tf.reduce_max(
+        target_q_values,
+        axis=1
+        )
+    mask = tf.one_hot(minibatch_actions, self.max_pebbles*self.max_piles)
+    with tf.GradientTape() as tape:
+            q_values = self.model(minibatch_states)
+            q_action = tf.reduce_sum(tf.multiply(q_values, mask), axis=1)
+            loss = self.loss_function(target_term, q_action)
+    self.cum_loss += loss
+    grads = tape.gradient(loss, self.model.trainable_variables)
+    self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+  def update_target(self):
+        self.model_target.set_weights(self.model.get_weights())
+  
+         
--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from Optimal import Optimal
 from Nim import Nim
 import numpy as np
 import matplotlib.pyplot as plt
+from DQN import DQN
+
+
+DQNAgent = DQN(5,3,0.01)
+opt = Optimal()
+DQN_vs_Opt = Nim(DQNAgent, opt, 10000)
+DQN_vs_Opt.play(State([4,3,2],3), False)
+
 
 def objective(trial):
   alpha = trial.suggest_float('alpha', 0.0, 1.0)
@@ -26,3 +34,45 @@ def objective(trial):
 
 study = optuna.create_study(direction='maximize')
 study.optimize(objective, n_trials=20)
+
+
+DQNagent = DQN(5,3,0)
+optAgent = Optimal()
+DQN_vs_opt = Nim(DQNagent, optAgent, 2000)
+
+
+def play(initState):
+    for i in np.arange(DQN_vs_opt.nGames):
+      currState = State(initState.state, initState.max_removal)
+      total_reward = 0
+      while True:
+        action_p1 = DQN_vs_opt.player1.act(currState)
+        next_state = currState.peekAction(Action((action_p1[0]-1,action_p1[1])))
+
+        if DQN_vs_opt.isCorrect(Action((action_p1[0]-1,action_p1[1])),currState):
+          DQN_vs_opt.num_correct_actions += 1
+        DQN_vs_opt.num_actions += 1
+        reward = 0
+
+        if action_p1 is None:
+          DQN_vs_opt.losses += 1
+          reward = -1
+          break
+        elif currState.isTerminal():
+          DQN_vs_opt.wins += 1
+          reward = 1
+          break
+        DQN_vs_opt.player1.on_step(currState, action_p1, reward, next_state, debug=False)
+        total_reward += reward
+        currState.doAction(Action((action_p1[0]-1,action_p1[1])))
+        if type(DQN_vs_opt.player2) is Q_Learning:
+          action_p2 = DQN_vs_opt.player2.updateQ(currState)
+        else:
+          action_p2 = DQN_vs_opt.player2.policy(currState)
+        currState.doAction(action_p2)
+      DQN_vs_opt.prop_list.append(DQN_vs_opt.num_correct_actions/DQN_vs_opt.num_actions)
+      if i % 100 == 0:
+        DQN_vs_opt.player1.update_target()
+      print(f"Episode {i+1}/{DQN_vs_opt.nGames} - Total Reward: {total_reward}")
+
+play(State([5,4,2],5))
